f7fb0bac480504bf2bb48b7b58f0c5433d8a2bc0
kahn-aar <nicolas.martinr@gmail.com>
2015-04-01 21:03:15 +0200

    Firebase and ngRoute

--- js/controllers.js
+++ js/controllers.js
@@ -1,48 +1,122 @@
-    var phonecatApp = angular.module('phonecatApp', []);
-     
-    phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-        function ($scope, $http) {
-            $http.get('./uv.json').success(function(data) {
-                $scope.listuv = data;
-            });
-            $http.get('./contenu.json').success(function(data) {
-                $scope.contenu = data;
-            });
-            $http.get('./xp.json').success(function(data) {
-                $scope.xp = data;
-            });
-            $http.get('./projets.json').success(function(data) {
-                $scope.projets = data;
-            });
+    var phonecatApp = angular.module('project', ['ngRoute', 'firebase']);
+
+    phonecatApp.value('fbURL', 'https://torrid-torch-6299.firebaseIO.com/')
+    .service('fbRef', function(fbURL) {
+        return new Firebase(fbURL)
+    })
+    .config(function($routeProvider) {
+        $routeProvider.when('/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/description.html',
+        })
+        .when('/admin/', {
+            controller:'AdminCtrl',
+            templateUrl:'admin.html'
+        })
+        .when('/desc/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/description.html'
+        })
+        .when('/xp/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/experience.html'
+        })
+        .when('/skills/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/competences.html'
+        })
+        .when('/projets/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/projets.html'
+        })
+        .when('/uvs/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/uvs.html'
+        })
+        .when('/autre/', {
+            controller:'PhoneListCtrl',
+            templateUrl:'partials/autrexp.html'
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+    });
+    phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', '$location', '$firebaseObject', '$firebaseArray', 'fbRef',
+        function ($scope, $http, $location, $firebaseObject, $firebaseArray, fbRef) {
+            console.log("hello");
+            $scope.infos = $firebaseObject(fbRef.child("Informations"));
+            $scope.contenu = $firebaseObject(fbRef.child("Contenu"));
+            $scope.xp = $firebaseArray(fbRef.child("Xp"));
+            $scope.otherXp = $firebaseObject(fbRef.child("AutreXp"));
+            $scope.listuv = $firebaseArray(fbRef.child("UVs"));
+            $scope.skills = $firebaseArray(fbRef.child("Skills"));
+            $scope.projets = $firebaseArray(fbRef.child("Projets"));
+            $scope.metrics = $firebaseObject(fbRef.child("Monitoring/projects"));
+            
+            $scope.displayProjets = function() {
+                var metrics = fbRef.child("Monitoring/projects");
+                var valuet = fbRef.child("Monitoring/projects/value");
+                valuet.once("value", function(data) {
+                    console.log(data.val());
+                    var newMetric = data.val() + 1;
+                    metrics.update({
+                        value: newMetric
+                    }).then(function(data) {
+                        $location.path('/projets/');
+                        $(".active").removeClass("active");
+                        $(".projectBtn").addClass("active");
+                    });
+                });
+            }
+
+            $scope.displayUv = function() {
+                $location.path('/uvs');
+                $(".active").removeClass("active");
+                $(".uvBtn").addClass("active");
+            }
+
+            $scope.displayContent = function() {
+                $location.path('/desc');
+                $(".active").removeClass("active");
+                $(".descriptionBtn").addClass("active");
+            }
+
+            $scope.displayXp = function() {
+                $location.path('/xp');
+                $(".active").removeClass("active");
+                $(".xpBtn").addClass("active");
+            }
+
+            $scope.displayOtherXp = function() {
+                $location.path('/autre');
+                $(".active").removeClass("active");
+                $(".otherXpBtn").addClass("active");
+            }
+
+            $scope.displaySkills = function() {
+                $location.path('/skills');
+                $(".active").removeClass("active");
+                $(".skillBtn").addClass("active");
+            }
+
+
+            $scope.query="";
         }
-    ]);
+    ])
+    .controller('AdminCtrl', function($scope, $firebaseObject) {
+        $scope.value = "hello world";
+        var ref = new Firebase("https://torrid-torch-6299.firebaseIO.com/Informations");
+        // download the data into a local object
+        $scope.infor = $firebaseObject(ref);
 
-    function displayUv() {
-        hideAll();
-        $('.uv').show(); 
-    }
+        $scope.save = function() {
+            console.log($scope.name);
+            ref.update({
+                text: $scope.name
+            });
+        };
+    });
 
-    function displayContent() {
-        hideAll();
-        $('.cont').show();
-    }
-
-    function displayXp() {
-        hideAll();
-        $('.xp').show(); 
-    }
-
-    function displayProjets() {
-        hideAll();
-        $('.projets').show(); 
-    }
-
-    function hideAll() {
-        $('.uv').hide(); 
-        $('.cont').hide();
-        $('.xp').hide();
-        $('.projets').hide();
-    }
-
+ 
 
     
--- readme.md
+++ readme.md
@@ -3,3 +3,11 @@ Site CV Angular
 
 *Ecrit par Nicolas Martin*
 
+Technologies
+-----------------------------
+HTML, CSS, JS, Angular
+
+Modifications
+------------------------------
+Il suffit de modifier les fichiers json afin de rentrer vos données
+
